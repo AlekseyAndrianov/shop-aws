793f853b9c47690fe7424f7c0499b6aaeead9074
AlekseyAndrianov <43870626+AlekseyAndrianov@users.noreply.github.com>
2020-04-08 19:07:38 +0300

    Remove custom sql queries

--- src/main/java/ru/shop/aws/controller/ShopController.java
+++ src/main/java/ru/shop/aws/controller/ShopController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.shop.aws.entity.Product;
 import ru.shop.aws.repository.ProductRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -14,19 +15,37 @@ public class ShopController {
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/{product_id}")
+    @GetMapping("/product/{product_id}")
     public Product getProduct(@PathVariable UUID product_id) {
         return productRepository.findById(product_id).get();
     }
-    @PostMapping("/new")
+
+    @GetMapping("/all")
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @DeleteMapping("/remove/{product_id}")
+    public UUID removeById(@PathVariable UUID product_id){
+        productRepository.deleteById(product_id);
+        return product_id;
+    }
+
+    @GetMapping("/all/count")
+    public long getCount(){
+        return productRepository.count();
+    }
+
+    @GetMapping("/new")
     public Product putProduct() {
         Product product = new Product();
-        product.setName(String.format("New product %d" + (productRepository.count() + 1)));
-        product.setArticle(String.format("New article %d" + (productRepository.count() + 1)));
+        product.setName(String.format("New product %d", (productRepository.count() + 1)));
+        product.setArticle(String.format("New article %d", (productRepository.count() + 1)));
         product.setColor("blue");
         product.setPrice(1000.0);
         product.setSize("M");
-        productRepository.save(product.getName(), product.getArticle(), product.getSize(), product.getColor(), product.getPrice());
+        productRepository.save(product);
+//        productRepository.save(product.getProduct_id(), product.getName(), product.getArticle(), product.getSize(), product.getColor(), product.getPrice());
         return product;
     }
 }
--- src/main/java/ru/shop/aws/repository/ProductRepository.java
+++ src/main/java/ru/shop/aws/repository/ProductRepository.java
@@ -1,25 +1,27 @@
 package ru.shop.aws.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.shop.aws.entity.Product;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
-    @Query("SELECT p FROM Product p where p.product_id = :product_id")
-    Optional<Product> findById(@Param("product_id") UUID product_id);
+//    @Query("SELECT p FROM Product p where p.product_id = :product_id")
+//    Optional<Product> findById(@Param("product_id") UUID product_id);
 
-    @Query(value = "INSERT INTO Product (name, article, size, color, price) values (:name, :article, :size, :color, :price)", nativeQuery = true)
-    <S extends Product> S save(
-            @Param("name") String name,
-            @Param("article") String article,
-            @Param("size") String size,
-            @Param("color") String color,
-            @Param("price") Double price);
+//    @Query(value = "INSERT INTO Product (product_id, name, article, size, color, price) values (:product_id, :name, :article, :size, :color, :price)", nativeQuery = true)
+//    <S extends Product> S save(
+//            @Param("product_id") UUID product_id,
+//            @Param("name") String name,
+//            @Param("article") String article,
+//            @Param("size") String size,
+//            @Param("color") String color,
+//            @Param("price") Double price);
+
+//    @Override
+//    @Query("select count(*) from Product")
+//    long count();
 }
